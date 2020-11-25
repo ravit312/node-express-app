d33c55f4645e34a5c5f4bd94f6874e0149a5cabc
raviteja.vigo <raviteja.ch@vigocare.com>
2020-11-25 09:49:15 +0530

    removed mongodb connection

--- app.js
+++ app.js
@@ -30,12 +30,21 @@ if (!isProduction) {
   app.use(errorhandler());
 }
 
-if(isProduction){
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect('mongodb://localhost/conduit');
-  mongoose.set('debug', true);
-}
+// if(isProduction){
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect('mongodb://localhost/conduit');
+//   mongoose.set('debug', true);
+// }
+
+
+app.get('/v1/health', (req, res, next) => {
+  res.status(200).send('OK')
+})
+app.get('/', (req, res, next) => {
+  res.status(200).send('Server running on port 3000')
+})
+
 
 require('./models/User');
 require('./models/Article');
